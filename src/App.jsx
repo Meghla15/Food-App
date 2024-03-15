6b6453c57cb69b5a1f605e4461a424720ca1dcc2
Meghla15 <meherunnesa1505@gmail.com>
2024-03-16 00:33:43 +0600

    json data passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Banner from './Banner'
 import Header from './Header'
+import SingleFood from './SingleFood';
+
 
 function App() {
+  const [foods, setFoods]=useState([]);
+  const[cart, setCart] = useState([]);
+
+  useEffect(()=>{
+    fetch("./fakeData.json")
+    .then(res => res.json())
+    .then(data => setFoods(data))
+  },[])
+
+   const handleClick =(f)=>{
+    const isExist =cart.find(fd => fd.id == f.id);
+    if (!isExist) {
+      setCart([...cart,f]);
+    }
+    else{
+     alert('already achi vaii')
+    }
+
+  }
+  console.log(cart)
   
 
   return (
@@ -14,8 +37,51 @@ function App() {
       <h1 className='text-3xl font-bold mt-10 text-center'>Our Recipes</h1>
       <p className='w-2/3 mx-auto text-center mt-2 text-slate-800'>Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque.  </p>
 
-      <div className=''>
+       
 
+         <div className='main-container flex lg:flex-row flex-col p-4 justify-between lg:mt-7'>
+         {
+            foods.map((fd)=><SingleFood 
+            food={fd}
+            handleClick={handleClick}></SingleFood>)
+          }
+          
+          
+
+          <div className='cart-container '>
+           <h1 className='text-3xl font-bold text-center'>Want to Cook </h1>
+           <div class="overflow-x-auto">
+                  <table class="table">
+            {/* head  */}
+           <thead>
+            <tr>
+           <th></th>
+           <th>Name</th>
+           <th>Types</th>
+           <th>Calories</th>
+           </tr>
+           </thead>
+           {
+            cart.map((item)=>{
+                 
+              <div>
+                <tbody>
+            {/* row 1 */}
+            <tr>
+            <th>1</th>
+           <td>{item.name}</td>
+           <td>Quality Control Specialist</td>
+           <td>Blue</td>
+            </tr>
+           </tbody>
+              </div>
+          
+            })
+           }
+             
+        </table>
+           </div>
+          </div>
       </div>
 
     </div>
---
+++ src/SingleFood.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const SingleFood = ({food,handleClick}) => {
+    // console.log(handleClick)
+    return (
+        <div className=''>
+          <div className='cards-container'>
+          <div className="card w-96 bg-base-100 shadow-xl">
+           <figure className="px-10 pt-10">
+            <img src={food.image} alt="" class="rounded-xl" />
+           </figure>
+           <div className="card-body text-start">
+            <h2 className="card-title">{food.name}</h2>
+             <p>{food.instructions}</p>
+             <hr className='border-[0.5px] border-slate-200 w-full ' />
+             <h1 className='text-xl font-semibold'>Ingredients : 6</h1>
+             <p>{food.ingredients}</p>
+             <hr className='border-[0.5px] border-slate-200 w-full ' />
+             <div className='flex justify-between'>
+              <div className='flex gap-2'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              <p>{food.prepTime} Min</p>
+              </div>
+               <div className='flex gap-2'>
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
+             <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
+                 </svg>
+                <p>{food.calories} Calories</p>
+
+               </div>
+
+             </div>
+            <div className="card-actions mt-3">
+             <button onClick={(e)=>handleClick(food)} className="btn bg-[#0BE58A] rounded-3xl w-2/3 mx-auto">Want to Cook </button>
+           </div>
+          </div>
+          </div>
+          </div>
+        </div>
+    );
+};
+
+export default SingleFood;
